562bec4f712d94180209db7c509865ace26e6d13
Venky0003 <rajv17270@gmail.com>
2023-05-20 17:32:40 +0530

    user register

---
+++ block-BNaaev/user-login/routes/users.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user')
+
+/* GET users listing. */
+router.get("/", function(req, res, next) {
+  res.render('users');
+});
+
+router.get("/register", (req, res, next) =>{
+  res.render('register')
+})
+router.post("/register",(req, res, next) =>{
+  User.create(req.body).then((user) =>{
+    res.redirect('/users/login')
+  }).catch(error => console.log(error))
+})
+
+router.get('/login', (req, res, next) =>{
+  res.render('login')
+})
+
+
+module.exports = router;
+
